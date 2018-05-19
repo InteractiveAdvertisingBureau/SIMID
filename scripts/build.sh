6f84afb03c81185d1c9e999d99b6c01694ea262f
Tim De Pauw <timdp@users.noreply.github.com>
2018-05-19 09:01:40 +0200

    Lint

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,9 +7,16 @@ OUTPUT=index.html
 
 cd "$( dirname "$0" )/.."
 
-echo -n "Building $OUTPUT ..."
-if curl https://api.csswg.org/bikeshed/ -F file=@"$INPUT" -F force=1 --silent >"$OUTPUT"; then
-  echo " Succeeded"
+echo -n "Linting $INPUT ..."
+errors="$( curl https://api.csswg.org/bikeshed/ -F file=@"$INPUT" -F force=1 -F output=err --silent )"
+
+if [[ -n $errors ]]; then
+  echo " Errors found."
+  echo -e "\n=== LINTER OUTPUT ===\n$errors\n=== END OF LINTER OUTPUT ===\n"
 else
-  echo " Failed"
+  echo " No errors found."
 fi
+
+echo -n "Building $OUTPUT ..."
+curl https://api.csswg.org/bikeshed/ -F file=@"$INPUT" -F force=1 --silent >"$OUTPUT"
+echo " Done."
