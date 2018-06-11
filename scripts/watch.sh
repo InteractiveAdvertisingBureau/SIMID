a60f03b233bcba09ae2022675144580b0ae0c238
Tim De Pauw <timdp@users.noreply.github.com>
2018-06-11 23:28:36 +0200

    editorial: make watch.sh compatible with Darwin (#46)

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -7,10 +7,14 @@ INPUT=index.bs
 dir="$( dirname "$0" )"
 cd "$dir/.."
 
-prev=$( stat -c %Z "$INPUT" )
+# If we can't invoke `stat` as GNU stat, fall back to BSD-style (incl. macOS)
+statopt='-c %Z'
+stat $statopt "$INPUT" >/dev/null 2>&1 || statopt='-f %Sm'
+
+prev=$( stat $statopt "$INPUT" )
 "$dir/build.sh"
 while true; do
-  curr=$( stat -c %Z "$INPUT" )
+  curr=$( stat $statopt "$INPUT" )
   if [[ $curr != $prev ]]; then
     echo "Change to $INPUT detected."
     "$dir/build.sh"
