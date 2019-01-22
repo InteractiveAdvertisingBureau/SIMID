a45ffab5a8b506a3e32b395f4adf7b57b44db933
Ryan Thompson <ryanthompson@ryanthompson.nyc.corp.google.com>
2019-01-22 10:50:31 -0500

    Added new videos for the sample player

--- examples/create_video_with_ffmpeg.sh
+++ examples/create_video_with_ffmpeg.sh
@@ -6,7 +6,15 @@ seconds=30;
 mantissaDigits=1;
 fontSize=200;
 fontFile="/usr/share/fonts/truetype/msttcorefonts/timesbd.ttf"
-ffmpeg -loop 1 -i black480.png -c:v libx264 \
+
+
+ffmpeg -loop 1 -i black480-Ad.png -c:v libx264 \
   -r $fps -t $seconds -pix_fmt yuv420p \
   -vf "fps=$fps,drawtext=fontfile=$fontFile:fontcolor=yellow:fontsize=$fontSize:x=(w-text_w)/2:y=(h-text_h)/2:text='%{eif\:($seconds-t)\:d}.%{eif\:(mod($seconds-t, 1)*pow(10,$mantissaDigits))\:d\:$mantissaDigits}'"\
-  black_countdown.mp4;
+  fakeAd.mp4;
+
+seconds=120;
+ffmpeg -loop 1 -i black480-Content.png -c:v libx264 \
+  -r $fps -t $seconds -pix_fmt yuv420p \
+  -vf "fps=$fps,drawtext=fontfile=$fontFile:fontcolor=green:fontsize=$fontSize:x=(w-text_w)/2:y=(h-text_h)/2:text='%{eif\:($seconds-t)\:d}.%{eif\:(mod($seconds-t, 1)*pow(10,$mantissaDigits))\:d\:$mantissaDigits}'"\
+  fakeContent.mp4;
