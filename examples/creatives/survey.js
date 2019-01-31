d00dd2966a37ef00e612c857bb1568add4d44261
Ryan Thompson <ryanthompson591@gmail.com>
2019-01-31 15:30:06 -0500

    Update sample player (#186)
    * Various fixes.  Adds a survey example.
    
    * adds fixes to the player so that init a new add does not cause other ad to still show
    
    * Added new videos for the sample player
    
    * Added new videos for the sample player
    
    * work in progress
    
    * fixed sample player to use a separate ad video element
    
    * updated survey creative and player
    
    * moved survey location

--- examples/creatives/survey.js
+++ examples/creatives/survey.js
@@ -6,48 +6,60 @@ class SivicSurvey extends BaseSivicCreative {
     super();
 
     /**
-     * The current Question being asked
+     * The current Question being asked.
+     * @private
      */
-    this.currentQuestion_ = 1;
+    this.currentQuestion_ = -1;
+
+    /**
+     * A list of questions to be asked. Default is no questions.
+     * @private
+     */
+    this.surveyQuestions_ = [];
 
     // listen to all buttons.
-    for (let i = 1; i <= 6; i++) {
+    for (let i = 0; i <= 2; i++) {
       const button = document.getElementById('option' + i);
       if (button) {
+        // This examples just shows the next question. A true
+        // implementation would record answers somehow.
         button.onclick = this.showNextQuestion.bind(this);
       }
     }
   }
 
-  showQuestion(questionNumber) {
-    const divRef = 'question' + questionNumber;
-    const ref = document.getElementById(divRef);
-    ref.classList.add('showing');
-  }
-
-  hideQuestion(questionNumber) {
-    const divRef = 'question' + questionNumber;
-    const ref = document.getElementById(divRef);
-    ref.classList.remove('showing');
+  /**
+   * Shows the current question
+   */
+  showQuestion() {
+    const questionData = this.surveyQuestions_[this.currentQuestion_];
+    const questionElement = document.getElementById('current-question');
+    questionElement.innerHTML = questionData.question;
+    for (let i = 0; i <= 2; i++) {
+      const button = document.getElementById('option' + i);
+      button.innerHTML = questionData.answers[i];
+    }
+    document.getElementById('question').classList.add('showing');
   }
 
   /** @override */
   onStart(eventData) {
     super.onStart(eventData);
-    this.showQuestion(1);
+    this.surveyQuestions_ = JSON.parse(this.creativeData.adParameters);
+    this.showNextQuestion();
   }
 
   /**
-   * Shows the next question
+   * Shows the next question.
    */
   showNextQuestion() {
-    this.hideQuestion(this.currentQuestion_);
+    document.getElementById('question').classList.remove('showing');
     this.currentQuestion_ ++;
-    if (this.currentQuestion_ == 3) {
-      // If the user answers all the questions skip the ad.
+    if (this.currentQuestion_ >= this.surveyQuestions_.length) {
+      // If the user answers all the questions skip the rest of the ad.
       this.sivicProtocol.sendMessage(CreativeMessage.REQUEST_SKIP);
       return;
     }
-    this.showQuestion(this.currentQuestion_);
+    setTimeout(() => this.showQuestion(), 1000);
   }
 }
