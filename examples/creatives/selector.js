94dfb619c0a598fb505929f1dadfb6166cabde96
Ryan Thompson <ryanthompson591@gmail.com>
2020-04-13 13:52:31 -0400

    added better comments to the selector ad

--- examples/creatives/selector.js
+++ examples/creatives/selector.js
@@ -15,29 +15,39 @@ class SimidSelector extends BaseSimidCreative {
     super.onStart(eventData);
     setTimeout(() => {
       this.showElements();
-      const video1 = document.getElementById('video1');
-      const video2 = document.getElementById('video2');
-
-      video1.addEventListener('click', () => this.onVideoClicked(video1, video2));
-      video2.addEventListener('click', () => this.onVideoClicked(video2, video1));
+      this.openVideoWhenClicked();
     }, 1000);
 
     // Give the user 10 seconds to select a shorter ad or continue with default ad.
     setTimeout(() => {
       if (!this.adSelected_) {
-        document.getElementById('title').classList.remove('showing');
-        document.getElementById('video1').classList.remove('showing');
-        document.getElementById('video2').classList.remove('showing');
+        this.hideElemenets();
       }
     }, 10000);
   }
 
+  /**
+   * Causes two videos and a title (text) element to animate over the video.
+   */
   showElements() {
     document.getElementById('title').classList.add('showing');
     document.getElementById('video1').classList.add('showing');
     document.getElementById('video2').classList.add('showing');
   }
 
+  /**
+   * Causes all overlayed elements to animate off the main video.
+   */
+  hideElements() {
+    document.getElementById('title').classList.remove('showing');
+    document.getElementById('video1').classList.remove('showing');
+    document.getElementById('video2').classList.remove('showing');
+  }
+
+  /**
+   * Asks the ad video to pause, plays the selected video and then ends the ad
+   * once the selected video is played
+   */
   onVideoClicked (clickedVid, unclickedVid) {
     console.log('vid clicked');
     this.adSelected_ = true;
@@ -54,6 +64,18 @@ class SimidSelector extends BaseSimidCreative {
 
     });
   }
+
+  /**
+   * Sets up click listeners on the video elements.  When a video element
+   * is clicked on, onVideo clicked is called.
+   */
+  openVideoWhenClicked() {
+      const video1 = document.getElementById('video1');
+      const video2 = document.getElementById('video2');
+
+      video1.addEventListener('click', () => this.onVideoClicked(video1, video2));
+      video2.addEventListener('click', () => this.onVideoClicked(video2, video1));
+    }
 
   onEnded() {
     this.simidProtocol.sendMessage(CreativeMessage.REQUEST_STOP);
