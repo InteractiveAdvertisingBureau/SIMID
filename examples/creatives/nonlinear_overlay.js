0279a805d6334300ccf453acfecd6b64e2e63442
Kaleab G <kaleabsolomon470@gmail.com>
2020-07-23 18:03:36 -0500

    Add top comments to js

--- examples/creatives/nonlinear_overlay.js
+++
@@ -1,9 +0,0 @@
-/**
- * A sample SIMID nonlinear ad that shows an overlay of
- * buttons to showcase functionality
- */
-class NonLinear extends BaseSimidCreative {
-    constructor() {
-      super();
-    }
-}
