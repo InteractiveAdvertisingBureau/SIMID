dac0c911c37848406c156de7d33f4738cc2f973a
juliareichel <juliareichel@google.com>
2020-07-23 09:48:52 -0400

    Set up nonlinear files.

---
+++ examples/creatives/nonlinear.js
@@ -0,0 +1,9 @@
+/**
+ * A sample SIMID nonlinear ad that shows an overlay of
+ * buttons to showcase functionality
+ */
+class NonLinear extends BaseSimidCreative {
+    constructor() {
+      super();
+    }
+}
