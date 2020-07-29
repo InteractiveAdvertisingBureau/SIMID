4a3c035f8a7a639d9bc1d80b1b330b3db5f3b385
kristen-m <kristenmason@google.com>
2020-07-29 16:34:30 -0500

    Basic banner ad setup

--- examples/player/simid_player.js
+++ examples/player/simid_player.js
@@ -120,7 +120,6 @@ class SimidPlayer {
    * a session.
    */
   initializeAd() {
-
     if (!this.isLinearAd_ && !this.isValidDimensions_(this.getNonlinearDimensions_())) {
       console.log('Unable to play a non-linear ad with dimensions bigger than the player. Please modify dimensions to a smaller size.');
       return;
@@ -149,6 +148,7 @@ class SimidPlayer {
     // is already created this will send the init message immediately.
     this.sessionCreatedPromise_.then(() => {
       this.sendInitMessage_();
+      debugger;
     });
 
   }
