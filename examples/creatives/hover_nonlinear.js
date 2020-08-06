916cd81edb76aea3415d72b0af3df9ec8fcb0373
juliareichel <juliareichel@google.com>
2020-08-06 10:53:16 -0400

    Figured out proper expanding.

--- examples/creatives/hover_nonlinear.js
+++ examples/creatives/hover_nonlinear.js
@@ -57,68 +57,165 @@ class HoverNonLinear extends BaseSimidCreative {
         const expandOnHoverFunction = () => {
             const newDimensions = {};
             let resizeParams = {};
+            // let desiredTopLeft;
+            // let desiredTopRight;
+            // let desiredMinY;
+            // let desiredMaxY;
+            // let desiredWidth;
+            // let desiredHeight;
+
+            let desiredX;
+            let desiredWidth;
+            let desiredY;
+            let desiredHeight;
+            let fullCreativeWidth;
+            let fullCreativeHeight;
+
+            desiredX = this.initialDimensions_.x * .8;
+            desiredWidth = this.initialDimensions_.width * 1.4;
+
+            if (desiredX <= 0) {
+                desiredX = 0;
+                desiredWidth = this.initialDimensions_.width * 1.2;
+            }
+            
+            if (desiredX > this.videoDimensionWidth) {
+                return;
+            }
+
+            fullCreativeWidth = desiredX + desiredWidth;
+
+            if (fullCreativeWidth > this.videoDimensionWidth) {
+                fullCreativeWidth = this.videoDimensionWidth;
+                desiredWidth = fullCreativeWidth - desiredX;
+            }
+            
+            desiredY = this.initialDimensions_.y * .8;
+            console.log("desired y: " + desiredY);
+            console.log("initial y " + this.initialDimensions_.y);
+            console.log("initial height: " + typeof(this.initialDimensions_.height));
+            desiredHeight = parseInt(this.initialDimensions_.height) + parseInt(this.initialDimensions_.y * .4);
+            console.log("desired height prior: " + desiredHeight);
+
+            if (desiredY <= 0) {
+                desiredY = 0;
+                desiredHeight = this.initialDimensions_.height * 1.2;
+            }
+            if (desiredY > this.videoDimensionHeight) {
+                return;
+            }
+
+            fullCreativeHeight = desiredY + desiredHeight;
+
+            if (fullCreativeHeight > this.videoDimensionHeight) {
+                console.log("HERE");
+                fullCreativeHeight = this.videoDimensionHeight;
+                desiredHeight = fullCreativeHeight - desiredY;
+            }
+
+            // desiredTopLeft = this.initialDimensions_.x - (this.initialDimensions_.width * 1.2);
+            // if (desiredTopLeft < 0) {
+            //     desiredTopLeft = 0;
+            // }
+            // desiredTopRight = this.initialDimensions_.x + (1.2 * this.initialDimensions_.width);
+            // if (desiredTopRight > this.videoDimensionWidth) {
+            //     desiredTopRight = this.videoDimensionWidth;
+            // }
+            // desiredWidth = desiredTopRight - desiredTopLeft;
+            // console.log(desiredWidth);
+
+            // desiredMinY = (this.initialDimensions_.height * 1.2) - this.initialDimensions_.y ;
+            // console.log(desiredMinY);
+            // if (desiredMinY < 0) {
+            //     desiredMinY = 0;
+            // }
+            // desiredMaxY = this.initialDimensions_.y + (1.2 * this.initialDimensions_.height);
+            // console.log(desiredMaxY);
+            // if (desiredMaxY > this.videoDimensionHeight) {
+            //     desiredMaxY = this.videoDimensionHeight;
+            // }
+
+            // desiredHeight = desiredMaxY - desiredMinY;
+            // console.log(desiredHeight);
+            console.log("x: " + desiredX);
+            console.log("Y: " + desiredY);
+            console.log("Width: " + desiredWidth);
+            console.log("Height: " + desiredHeight);
+
+            newDimensions.x = desiredX;
+            newDimensions.y = desiredY;
+            newDimensions.width = desiredWidth;
+            newDimensions.height = desiredHeight;
+
+            resizeParams = {
+                creativeDimensions: newDimensions,
+                videoDimensions: this.environmentData.videoDimensions,
+            };
+            
+            this.requestResize(resizeParams);
 
             //if ad is at the top quarter of the player
-            if (this.initialDimensions_.y < (this.videoDimensionHeight * QUARTER_THE_SIZE)) {
-                newDimensions.height = (this.videoDimensionHeight * THIRTY_FIVE_PERCENT);
-                newDimensions.x = 0;
-                newDimensions.y = this.initialDimensions_.y;
-                newDimensions.width = this.videoDimensionWidth;
+            // if (this.initialDimensions_.y < (this.videoDimensionHeight * QUARTER_THE_SIZE)) {
+            //     newDimensions.height = (this.videoDimensionHeight * THIRTY_FIVE_PERCENT);
+            //     newDimensions.x = 0;
+            //     newDimensions.y = this.initialDimensions_.y;
+            //     newDimensions.width = this.videoDimensionWidth;
 
-                resizeParams = {
-                    creativeDimensions: newDimensions,
-                    videoDimensions: this.environmentData.videoDimensions,
-                };
+            //     resizeParams = {
+            //         creativeDimensions: newDimensions,
+            //         videoDimensions: this.environmentData.videoDimensions,
+            //     };
             
-                this.requestResize(resizeParams);
-            }
+            //     this.requestResize(resizeParams);
+            // }
 
-            //if ad is between the top quarter and half of the player
-            else if (this.initialDimensions_.y > (this.videoDimensionHeight * QUARTER_THE_SIZE) && 
-                this.initialDimensions_.y < (this.videoDimensionHeight * HALF_THE_SIZE)) {
-                    newDimensions.height = (this.videoDimensionHeight * THIRTY_FIVE_PERCENT);
-                    newDimensions.x = 0;
-                    newDimensions.y = this.videoDimensionHeight * QUARTER_THE_SIZE;
-                    newDimensions.width = this.videoDimensionWidth;
+            // //if ad is between the top quarter and half of the player
+            // else if (this.initialDimensions_.y > (this.videoDimensionHeight * QUARTER_THE_SIZE) && 
+            //     this.initialDimensions_.y < (this.videoDimensionHeight * HALF_THE_SIZE)) {
+            //         newDimensions.height = (this.videoDimensionHeight * THIRTY_FIVE_PERCENT);
+            //         newDimensions.x = 0;
+            //         newDimensions.y = this.videoDimensionHeight * QUARTER_THE_SIZE;
+            //         newDimensions.width = this.videoDimensionWidth;
 
-                    resizeParams = {
-                        creativeDimensions: newDimensions,
-                        videoDimensions: this.environmentData.videoDimensions,
-                    };
+            //         resizeParams = {
+            //             creativeDimensions: newDimensions,
+            //             videoDimensions: this.environmentData.videoDimensions,
+            //         };
                 
-                    this.requestResize(resizeParams);
-            }
+            //         this.requestResize(resizeParams);
+            // }
 
-            //if ad is between the top half and three quarters of the player
-            else if (this.initialDimensions_.y > (this.videoDimensionHeight * HALF_THE_SIZE) && 
-            this.initialDimensions_.y < (this.videoDimensionHeight * THREE_QUARTERS_THE_SIZE)) {
-                newDimensions.height = (this.videoDimensionHeight * THIRTY_FIVE_PERCENT);
-                newDimensions.x = 0;
-                newDimensions.y = (this.videoDimensionHeight * HALF_THE_SIZE);
-                newDimensions.width = this.videoDimensionWidth;
+            // //if ad is between the top half and three quarters of the player
+            // else if (this.initialDimensions_.y > (this.videoDimensionHeight * HALF_THE_SIZE) && 
+            // this.initialDimensions_.y < (this.videoDimensionHeight * THREE_QUARTERS_THE_SIZE)) {
+            //     newDimensions.height = (this.videoDimensionHeight * THIRTY_FIVE_PERCENT);
+            //     newDimensions.x = 0;
+            //     newDimensions.y = (this.videoDimensionHeight * HALF_THE_SIZE);
+            //     newDimensions.width = this.videoDimensionWidth;
 
-                resizeParams = {
-                    creativeDimensions: newDimensions,
-                    videoDimensions: this.environmentData.videoDimensions,
-                };
+            //     resizeParams = {
+            //         creativeDimensions: newDimensions,
+            //         videoDimensions: this.environmentData.videoDimensions,
+            //     };
             
-                this.requestResize(resizeParams);
-            }
+            //     this.requestResize(resizeParams);
+            // }
 
-            //if ad is at bottom quarter of the player
-            else if (this.initialDimensions_.y >= (this.videoDimensionHeight * THREE_QUARTERS_THE_SIZE)) {
-                newDimensions.height = (this.videoDimensionHeight * QUARTER_THE_SIZE);
-                newDimensions.x = 0;
-                newDimensions.y = (this.videoDimensionHeight * THREE_QUARTERS_THE_SIZE);
-                newDimensions.width = this.videoDimensionWidth;
+            // //if ad is at bottom quarter of the player
+            // else if (this.initialDimensions_.y >= (this.videoDimensionHeight * THREE_QUARTERS_THE_SIZE)) {
+            //     newDimensions.height = (this.videoDimensionHeight * QUARTER_THE_SIZE);
+            //     newDimensions.x = 0;
+            //     newDimensions.y = (this.videoDimensionHeight * THREE_QUARTERS_THE_SIZE);
+            //     newDimensions.width = this.videoDimensionWidth;
 
-                resizeParams = {
-                    creativeDimensions: newDimensions,
-                    videoDimensions: this.environmentData.videoDimensions,
-                };
+            //     resizeParams = {
+            //         creativeDimensions: newDimensions,
+            //         videoDimensions: this.environmentData.videoDimensions,
+            //     };
             
-                this.requestResize(resizeParams);
-            }
+            //     this.requestResize(resizeParams);
+            // }
+
         }
         document.getElementById(elementName).addEventListener(event, expandOnHoverFunction);
     }
