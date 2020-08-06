20d3aabd21f2b893ff40bb5cab536bd483ed1fdb
juliareichel <juliareichel@google.com>
2020-08-06 11:23:04 -0400

    Working logic.

--- examples/creatives/hover_nonlinear.js
+++ examples/creatives/hover_nonlinear.js
@@ -3,20 +3,22 @@ const THIRTY_FIVE_PERCENT = .35;
 const HALF_THE_SIZE = .5;
 const THREE_QUARTERS_THE_SIZE = .75;
 
-/* This creative expands and collapses when the user hovers over the banner. */
+/** This creative expands and collapses when the user hovers over the banner. */
 class HoverNonLinear extends BaseSimidCreative {
     constructor() {
         super();
 
         this.initialDimensions_ = null;
+        this.videoDimensionWidth_ = null;
+        this.videoDimensionHeight_ = null;
     }
 
     /** @override */
     onInit(eventData) {
         super.onInit(eventData);
         this.initialDimensions_ = this.environmentData.creativeDimensions;
-        this.videoDimensionHeight = this.environmentData.videoDimensions.height;
-        this.videoDimensionWidth = this.environmentData.videoDimensions.width;
+        this.videoDimensionHeight_ = this.environmentData.videoDimensions.height;
+        this.videoDimensionWidth_ = this.environmentData.videoDimensions.width;
     }
 
     /** @override */
@@ -57,12 +59,6 @@ class HoverNonLinear extends BaseSimidCreative {
         const expandOnHoverFunction = () => {
             const newDimensions = {};
             let resizeParams = {};
-            // let desiredTopLeft;
-            // let desiredTopRight;
-            // let desiredMinY;
-            // let desiredMaxY;
-            // let desiredWidth;
-            // let desiredHeight;
 
             let desiredX;
             let desiredWidth;
@@ -71,72 +67,43 @@ class HoverNonLinear extends BaseSimidCreative {
             let fullCreativeWidth;
             let fullCreativeHeight;
 
-            desiredX = this.initialDimensions_.x * .8;
-            desiredWidth = this.initialDimensions_.width * 1.4;
+            desiredX = this.initialDimensions_.x * .6;
+            desiredWidth = parseInt(this.initialDimensions_.width) + parseInt(this.initialDimensions_.x * .6);
 
             if (desiredX <= 0) {
                 desiredX = 0;
                 desiredWidth = this.initialDimensions_.width * 1.2;
             }
             
-            if (desiredX > this.videoDimensionWidth) {
+            if (desiredX > this.videoDimensionWidth_) {
                 return;
             }
 
             fullCreativeWidth = desiredX + desiredWidth;
 
-            if (fullCreativeWidth > this.videoDimensionWidth) {
-                fullCreativeWidth = this.videoDimensionWidth;
+            if (fullCreativeWidth > this.videoDimensionWidth_) {
+                fullCreativeWidth = this.videoDimensionWidth_;
                 desiredWidth = fullCreativeWidth - desiredX;
             }
             
-            desiredY = this.initialDimensions_.y * .8;
-            console.log("desired y: " + desiredY);
-            console.log("initial y " + this.initialDimensions_.y);
-            console.log("initial height: " + typeof(this.initialDimensions_.height));
-            desiredHeight = parseInt(this.initialDimensions_.height) + parseInt(this.initialDimensions_.y * .4);
-            console.log("desired height prior: " + desiredHeight);
+            desiredY = this.initialDimensions_.y * .6;
+            desiredHeight = parseInt(this.initialDimensions_.height) + parseInt(this.initialDimensions_.y * .6);
 
             if (desiredY <= 0) {
                 desiredY = 0;
                 desiredHeight = this.initialDimensions_.height * 1.2;
             }
-            if (desiredY > this.videoDimensionHeight) {
+            if (desiredY > this.videoDimensionHeight_) {
                 return;
             }
 
             fullCreativeHeight = desiredY + desiredHeight;
 
-            if (fullCreativeHeight > this.videoDimensionHeight) {
-                console.log("HERE");
-                fullCreativeHeight = this.videoDimensionHeight;
+            if (fullCreativeHeight > this.videoDimensionHeight_) {
+                fullCreativeHeight = this.videoDimensionHeight_;
                 desiredHeight = fullCreativeHeight - desiredY;
             }
 
-            // desiredTopLeft = this.initialDimensions_.x - (this.initialDimensions_.width * 1.2);
-            // if (desiredTopLeft < 0) {
-            //     desiredTopLeft = 0;
-            // }
-            // desiredTopRight = this.initialDimensions_.x + (1.2 * this.initialDimensions_.width);
-            // if (desiredTopRight > this.videoDimensionWidth) {
-            //     desiredTopRight = this.videoDimensionWidth;
-            // }
-            // desiredWidth = desiredTopRight - desiredTopLeft;
-            // console.log(desiredWidth);
-
-            // desiredMinY = (this.initialDimensions_.height * 1.2) - this.initialDimensions_.y ;
-            // console.log(desiredMinY);
-            // if (desiredMinY < 0) {
-            //     desiredMinY = 0;
-            // }
-            // desiredMaxY = this.initialDimensions_.y + (1.2 * this.initialDimensions_.height);
-            // console.log(desiredMaxY);
-            // if (desiredMaxY > this.videoDimensionHeight) {
-            //     desiredMaxY = this.videoDimensionHeight;
-            // }
-
-            // desiredHeight = desiredMaxY - desiredMinY;
-            // console.log(desiredHeight);
             console.log("x: " + desiredX);
             console.log("Y: " + desiredY);
             console.log("Width: " + desiredWidth);
@@ -153,69 +120,6 @@ class HoverNonLinear extends BaseSimidCreative {
             };
             
             this.requestResize(resizeParams);
-
-            //if ad is at the top quarter of the player
-            // if (this.initialDimensions_.y < (this.videoDimensionHeight * QUARTER_THE_SIZE)) {
-            //     newDimensions.height = (this.videoDimensionHeight * THIRTY_FIVE_PERCENT);
-            //     newDimensions.x = 0;
-            //     newDimensions.y = this.initialDimensions_.y;
-            //     newDimensions.width = this.videoDimensionWidth;
-
-            //     resizeParams = {
-            //         creativeDimensions: newDimensions,
-            //         videoDimensions: this.environmentData.videoDimensions,
-            //     };
-            
-            //     this.requestResize(resizeParams);
-            // }
-
-            // //if ad is between the top quarter and half of the player
-            // else if (this.initialDimensions_.y > (this.videoDimensionHeight * QUARTER_THE_SIZE) && 
-            //     this.initialDimensions_.y < (this.videoDimensionHeight * HALF_THE_SIZE)) {
-            //         newDimensions.height = (this.videoDimensionHeight * THIRTY_FIVE_PERCENT);
-            //         newDimensions.x = 0;
-            //         newDimensions.y = this.videoDimensionHeight * QUARTER_THE_SIZE;
-            //         newDimensions.width = this.videoDimensionWidth;
-
-            //         resizeParams = {
-            //             creativeDimensions: newDimensions,
-            //             videoDimensions: this.environmentData.videoDimensions,
-            //         };
-                
-            //         this.requestResize(resizeParams);
-            // }
-
-            // //if ad is between the top half and three quarters of the player
-            // else if (this.initialDimensions_.y > (this.videoDimensionHeight * HALF_THE_SIZE) && 
-            // this.initialDimensions_.y < (this.videoDimensionHeight * THREE_QUARTERS_THE_SIZE)) {
-            //     newDimensions.height = (this.videoDimensionHeight * THIRTY_FIVE_PERCENT);
-            //     newDimensions.x = 0;
-            //     newDimensions.y = (this.videoDimensionHeight * HALF_THE_SIZE);
-            //     newDimensions.width = this.videoDimensionWidth;
-
-            //     resizeParams = {
-            //         creativeDimensions: newDimensions,
-            //         videoDimensions: this.environmentData.videoDimensions,
-            //     };
-            
-            //     this.requestResize(resizeParams);
-            // }
-
-            // //if ad is at bottom quarter of the player
-            // else if (this.initialDimensions_.y >= (this.videoDimensionHeight * THREE_QUARTERS_THE_SIZE)) {
-            //     newDimensions.height = (this.videoDimensionHeight * QUARTER_THE_SIZE);
-            //     newDimensions.x = 0;
-            //     newDimensions.y = (this.videoDimensionHeight * THREE_QUARTERS_THE_SIZE);
-            //     newDimensions.width = this.videoDimensionWidth;
-
-            //     resizeParams = {
-            //         creativeDimensions: newDimensions,
-            //         videoDimensions: this.environmentData.videoDimensions,
-            //     };
-            
-            //     this.requestResize(resizeParams);
-            // }
-
         }
         document.getElementById(elementName).addEventListener(event, expandOnHoverFunction);
     }
